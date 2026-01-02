87fe21401dcc10dbfb02ca8b3e2817c9a727c43f
JVbNjlefwgwtr8 <CIOrf345klDje45845hFk34ufke5@proton.me>
2026-01-02 12:24:10 +0000

    main changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,23 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <Script
-          id="orchids-browser-logs"
-          src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts/orchids-browser-logs.js"
-          strategy="afterInteractive"
-          data-orchids-project-id="180cb890-327d-4304-9866-a2150aafa63e"
-        />
         <ErrorReporter />
-        <Script
-          src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts//route-messenger.js"
-          strategy="afterInteractive"
-          data-target-origin="*"
-          data-message-type="ROUTE_CHANGE"
-          data-include-search-params="true"
-          data-only-in-iframe="true"
-          data-debug="true"
-          data-custom-data='{"appName": "YourApp", "version": "1.0.0", "greeting": "hi"}'
-        />
         <AuthProvider>
           <StoreProvider>
             {children}
